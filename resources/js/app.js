6c6d344ecf972fe407b443a99a641fcb33ec2b04
Nigora <nigor@bitbucket.org>
2019-03-01 19:18:28 +0500

    vue + role

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,46 @@
 
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * First, we will load all of this project's Javascript utilities and other
+ * dependencies. Then, we will be ready to develop a robust and powerful
+ * application frontend using useful Laravel and JavaScript libraries.
  */
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue'
+import App from './App.vue'
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+// Vuesax Component Framework
+import Vuesax from 'vuesax'
+import 'material-icons/iconfont/material-icons.css' //Material Icons
+import 'vuesax/dist/vuesax.css'; // Vuesax
+Vue.use(Vuesax)
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+// Theme Configurations
+import '../../themeConfig.js'
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Globally Registered Components
+import '../globalComponents.js'
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+// Styles: SCSS
+import '@/assets/scss/main.scss'
 
-const app = new Vue({
-    el: '#app'
-});
+// Tailwind
+import '@/assets/css/main.css';
+
+// Vue Router
+import router from './router'
+
+// Vuex Store
+import store from '../store/store'
+
+// Feather font icon
+require('@/assets/css/iconfont.css')
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
